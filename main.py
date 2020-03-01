54ac9138e803d06cb68b606482485ed7945256b5
NONAME <zycs@yandex.ru>
2020-03-01 23:13:53 +0300

    Rewrite class Home and Cat Write class Human and create human Bill. Write function go_home and get_cat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 class Cat:
-
     def __init__(self, name):
         self.name = name
         self.meal = 30
@@ -9,12 +8,9 @@ class Cat:
         return '{} - сытость: {} \tдом: {}'.format(self.name, self.meal, self.home)
 
 
-Kimbo = Cat('Kimbo')
-print(Kimbo)
-
-
 class Home:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.bowl = None
         self.dirty = False
         self.meal = 50
@@ -28,5 +24,43 @@ class Home:
         return 'В доме {}, корма: {}, еды: {}, денег: {}'.format(dirt, self.bowl, self.meal, self.money)
 
 
-sweet_home = Home()
-print(sweet_home)
+class Human:
+    def __init__(self, name):
+        self.name = name
+        self.hunger = 30
+        self.home = None
+
+    def __str__(self):
+        return '{} живет в {} - сытость: {} '.format(self.name, self.home, self.hunger)
+
+    def go_home(self, home):
+        self.home = home.name
+        home.dirty = False
+        print('{} вьехал в дом {}'.format(self.name, self.home))
+
+    def get_cat(self, cat, home):
+        cat.home = home.name
+        home.bowl = 20
+        home.dirty = True
+
+
+my_home = Home(name='My')
+print('Построили дом')
+print(my_home)
+
+Bill = Human(name='Bill')
+print('Создали человека')
+print(Bill)
+
+Bill.go_home(home=my_home)
+print(Bill)
+print(my_home)
+print('------------------------------')
+
+Kimbo = Cat(name='Kimbo')
+print('Родился кот')
+print(Kimbo)
+
+Bill.get_cat(cat=Kimbo, home=my_home)
+print('Поселили {} в дом {}'.format(Kimbo.name, my_home.name))
+print(my_home)
