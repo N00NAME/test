2be4eb48710b8b18976fdc18da87a03a06410c47
NONAME <zycs@yandex.ru>
2020-03-01 22:07:46 +0300

    Write class Home and create home sweet_home

--- main.py
+++ main.py
@@ -6,8 +6,27 @@ class Cat:
         self.home = None
 
     def __str__(self):
-        return ('{} - сытость: {} \tдом: {}'.format(self.name, self.meal, self.home))
+        return '{} - сытость: {} \tдом: {}'.format(self.name, self.meal, self.home)
 
 
 Kimbo = Cat('Kimbo')
 print(Kimbo)
+
+
+class Home:
+    def __init__(self):
+        self.bowl = None
+        self.dirty = False
+        self.meal = 50
+        self.money = 100
+
+    def __str__(self):
+        if self.dirty:
+            dirt = 'грязно'
+        else:
+            dirt = 'чисто'
+        return 'В доме {}, корма: {}, еды: {}, денег: {}'.format(dirt, self.bowl, self.meal, self.money)
+
+
+sweet_home = Home()
+print(sweet_home)
