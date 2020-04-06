2597b0039dbb84a84a5087f5d484d454e8af08f8
NONAME <zycs@yandex.ru>
2020-04-06 13:10:49 +0300

    Убрал в отдельные файлы:     Вывод и форматирование     Работа с файлами     Списки Очистил probe.py

---
+++ Вывод и форматирование.py
@@ -0,0 +1,47 @@
+
+# name = input("Как вас зовут? ")
+# print('Привет, {}!'.format(name))
+
+
+# for i in 'Hello world!':
+#     if i == ' ':
+#         break
+#     elif i == 'H':
+#         continue
+#     print(i * 2, end='')
+
+
+# print(r'www.1st.com/download/version1_10')
+# print(r'C:\temp\new')
+# print('''\nМуха села
+# # на варенье
+# вот и все
+# стихотворенье''')
+# print("s \np \ta \nbbb")
+
+
+# S = 'counter-terrorist'
+# A = 'Andrew, Tony, Tomas'
+# print(S.split('-'))
+# print(A.split(','))
+# print(S.upper())
+# print(S.lower())
+# print(S.capitalize())
+# print(A.swapcase())
+
+
+# coordinates = {'y': 10, 'x': 5}
+# print('Coordinates: y={y}, x={x}'.format(**coordinates))
+
+
+# print('{:<20}'.format('left aligned'))
+# print('{:^20}'.format('center aligned'))
+# print('{:>20}'.format('right aligned'))
+
+
+# print('{:+f}; {:+f}'.format(3.14, -3.14))
+# print('{: f}; {: f}'.format(3.14, -3.14))
+
+
+# # % умножает число на 100!
+# print('answers: {:.2%}'.format(8 / 11))
